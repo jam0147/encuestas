570a19f4b3360a90e1bbde210e23aeefef109cd8
Rodolfo <rodolfopalacio487@gmail.com>
2018-04-25 11:55:37 -0400

    prueba

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    You are logged in! asd
                 </div>
             </div>
         </div>
