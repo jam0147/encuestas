45df90b1b2b554c0f2904d45cd0de4b13b4ef91d
Rodolfo <rodolfopalacio487@gmail.com>
2018-04-25 11:58:26 -0400

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    You are logged in! asd
+                    You are logged in! asd asd
                 </div>
             </div>
         </div>
